5a1b63045af3fba15e160a2cf04e54e5272a256d
Scala Steward <me@scala-steward.org>
2022-04-07 01:38:51 +0200

    Reformat with scalafmt 3.5.1

--- src/main/scala/com/thoughtworks/Example.scala
+++ src/main/scala/com/thoughtworks/Example.scala
@@ -23,16 +23,11 @@ import scala.reflect.NameTransformer
   *
   * ==Step 1: Add this plug-in in your sbt settings==
   *
-  * `<pre>
-  * // project/plugins.sbt
-  * addSbtPlugin("com.thoughtworks.example" % "sbt-example" % "latest.release")
-  * </pre>`
+  * `<pre> // project/plugins.sbt addSbtPlugin("com.thoughtworks.example" %
+  * "sbt-example" % "latest.release") </pre>`
   *
-  * `<pre>
-  * // build.sbt
-  * enablePlugins(Example)
-  * libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.10" % Test
-  * </pre>`
+  * `<pre> // build.sbt enablePlugins(Example) libraryDependencies +=
+  * "org.scalatest" %% "scalatest" % "3.2.10" % Test </pre>`
   *
   * ==Step 2: Run tests==
   *
