7d1d8a25390633cdcd83a5d544344822e28271d3
Yang, Bo <pop.atry@gmail.com>
2023-01-09 13:03:13 -0800

    Generate example test in test dialect (#185)

---
+++ src/sbt-test/sbt-example/scala3-keyword-then/src/main/scala/mypackage/Scala3KeywordThen.scala
@@ -0,0 +1,11 @@
+
+
+/** Testing Scala 3 keyword
+  * @example
+  *   Call to `then` should compile
+  *   {{{
+  *   import scala.scalajs.js
+  *   js.Promise.resolve(40).`then`(_ + 2)
+  *   }}}
+  */
+object Scala3KeywordThen
